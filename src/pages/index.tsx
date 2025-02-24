21c999d31ab530b3126f21347c80add9cb0d3022
cyd5538 <cyd5538@naver.com>
2025-02-24 16:31:28 +0900

    Design: main css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,18 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+        <div className={clsx(styles.buttons, "mt-6 flex gap-4 justify-center")}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg shadow-md"
             to="https://github.com/cyd5538">
-            My github ✨
+            🚀 My GitHub
           </Link>
+          <Link
+            className="button button--outline shadow-md"
+            to="/docs/category/자바스크립트">
+            📚 Get Started
+          </Link>
+        </div>
         </div>
       </div>
     </header>
@@ -35,9 +42,6 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
